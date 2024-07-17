cc3013bdaad2542696033bfbbde1fcda78cb3648
pavan <pavangolla7@gmail.com>
2024-07-17 00:10:00 -0400

    Initial commit on develop branch

---
+++ src/main/java/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+public class ao {
+	public static void main(String[] args) {
+		int num1=20;
+		int num2=10;
+		System.out.println(num1+"+"+num2+"="+(num1+num2));
+		System.out.println(num1+"-"+num2+"="+(num1-num2));
+		System.out.println(num1+"*"+num2+"="+(num1*num2));
+		System.out.println(num1+"/"+num2+"="+(num1/num2));
+		System.out.println(num1+"%"+num2+"="+(num1%num2));
+		
+	}
+}
